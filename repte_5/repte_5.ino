f03421d4ecbca1d8d97b6c1ba0786e6a34f28f18
Gorkabmc <gorkabmc@gmail.com>
2017-03-11 20:43:54 +0100

    Array de 8 LEDs 5

---
+++ repte_5/repte_5.ino
@@ -0,0 +1,182 @@
+/**********************************************************************************
+**                                                                               **
+**                     Repte 1. Array de 8 LEDs 5                                **
+**                                                                               **
+** GORKA FERNÁNDEZ RAMOS                                       11/03/2017        **
+**********************************************************************************/
+
+//******************************* INCLUDES ****************************************
+
+//******************************* VARIABLES ***************************************
+int LED5 = 5;
+int LED6 = 6;
+int LED7 = 7;
+int LED8 = 8;
+int B;
+//******************************** SETUP ******************************************
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Entra numero !");
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+//********************************* LOOP ******************************************
+void loop()
+{
+  while (Serial.available() > 0) {
+    B = Serial.parseInt();
+    if (Serial.read() == '\n') {
+      if (B == 0)
+      {
+      }
+      else if (B == 1)
+      {
+        digitalWrite(5, HIGH);
+        delay(1000);
+        digitalWrite(5, LOW);
+        delay(1000);
+      }
+      else if (B == 2)
+      {
+        digitalWrite(6, HIGH);
+        delay(1000);
+        digitalWrite(6, LOW);
+        delay(1000);
+      }
+      else if (B == 3)
+      {
+        digitalWrite(5, HIGH);
+        digitalWrite(6, HIGH);
+        delay(1000);
+        digitalWrite(5, LOW);
+        digitalWrite(6, LOW);
+        delay(1000);
+      }
+      else if (B == 4)
+      {
+        digitalWrite(7, HIGH);
+        delay(1000);
+        digitalWrite(7, LOW);
+        delay(1000);
+      }
+      else if (B == 5)
+      {
+        digitalWrite(5, HIGH);
+        digitalWrite(7, HIGH);
+        delay(1000);
+        digitalWrite(5, LOW);
+        digitalWrite(7, LOW);
+        delay(1000);
+      }
+      else if (B == 6)
+      {
+
+        digitalWrite(6, HIGH);
+        digitalWrite(7, HIGH);
+        delay(1000);
+
+        digitalWrite(6, LOW);
+        digitalWrite(7, LOW);
+        delay(1000);
+      }
+      else if (B == 7)
+      {
+        digitalWrite(5, HIGH);
+        digitalWrite(6, HIGH);
+        digitalWrite(7, HIGH);
+        delay(1000);
+        digitalWrite(5, LOW);
+        digitalWrite(6, LOW);
+        digitalWrite(7, LOW);
+        delay(1000);
+      }
+      else if (B == 8)
+      {
+        digitalWrite(8, HIGH);
+        delay(1000);
+        digitalWrite(8, LOW);
+        delay(1000);
+      }
+      else if (B == 9)
+      {
+        digitalWrite(8, HIGH);
+        digitalWrite(5, HIGH);
+        delay(1000);
+        digitalWrite(8, LOW);
+        digitalWrite(5, LOW);
+        delay(1000);
+      }
+      else if (B == 10)
+      {
+        digitalWrite(8, HIGH);
+        digitalWrite(6, HIGH);
+        delay(1000);
+        digitalWrite(8, LOW);
+        digitalWrite(6, LOW);
+        delay(1000);
+      }
+      else if (B == 11)
+      {
+        digitalWrite(8, HIGH);
+        digitalWrite(6, HIGH);
+        digitalWrite(5, HIGH);
+        delay(1000);
+        digitalWrite(8, LOW);
+        digitalWrite(6, LOW);
+        digitalWrite(5, LOW);
+        delay(1000);
+      }
+      else if (B == 12)
+      {
+        digitalWrite(8, HIGH);
+        digitalWrite(7, HIGH);
+        delay(1000);
+        digitalWrite(8, LOW);
+        digitalWrite(7, LOW);
+        delay(1000);
+      }
+      else if (B == 13)
+      {
+        digitalWrite(8, HIGH);
+        digitalWrite(7, HIGH);
+        digitalWrite(5, HIGH);
+        delay(1000);
+        digitalWrite(8, LOW);
+        digitalWrite(7, LOW);
+        digitalWrite(5, LOW);
+        delay(1000);
+      }
+      else if (B == 14)
+      {
+        digitalWrite(8, HIGH);
+        digitalWrite(7, HIGH);
+        digitalWrite(6, HIGH);
+        delay(1000);
+        digitalWrite(8, LOW);
+        digitalWrite(7, LOW);
+        digitalWrite(6, LOW);
+        delay(1000);
+      }
+      else if (B == 15)
+      {
+        digitalWrite(8, HIGH);
+        digitalWrite(7, HIGH);
+        digitalWrite(6, HIGH);
+        digitalWrite(5, HIGH);
+        delay(1000);
+        digitalWrite(8, LOW);
+        digitalWrite(7, LOW);
+        digitalWrite(6, LOW);
+        digitalWrite(5, LOW);
+        delay(1000);
+      }
+    }
+  }
+}
+
+
+//******************************* FUNCIONS ****************************************
