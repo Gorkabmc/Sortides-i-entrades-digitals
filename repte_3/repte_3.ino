29b263781448c3a4f0ddd1f6313b95b4d383549f
Gorkabmc <gorkabmc@gmail.com>
2017-03-11 19:07:19 +0100

    Array de 8 LEDs 3

--- repte_3/repte_3.ino
+++ repte_3/repte_3.ino
@@ -107,24 +107,32 @@ void loop()
   digitalWrite(5, LOW);
   delay(50);
 
-  digitalWrite(8 Y 10, HIGH);
+  digitalWrite(8 , HIGH);
+  digitalWrite(9 , HIGH);
   delay(80);
-  digitalWrite(8 Y 10, LOW);
+  digitalWrite(8 , LOW);
+  digitalWrite(9 , LOW);
   delay(80);
 
   digitalWrite(7 , HIGH);
+  digitalWrite(10 , HIGH);
   delay(80);
   digitalWrite(7 , LOW);
+    digitalWrite(10 , LOW);
   delay(80);
 
     digitalWrite(6, HIGH);
+    digitalWrite(11, HIGH);
   delay(80);
   digitalWrite(6, LOW);
+    digitalWrite(11, LOW);
   delay(80);
 
   digitalWrite(5, HIGH);
+  digitalWrite(12, HIGH);
   delay(80);
   digitalWrite(5, LOW);
+  digitalWrite(12, LOW);
   delay(80);
 }
 
