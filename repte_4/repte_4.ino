fceaa02dfdc920b091b7a8555144541bd332b9cc
Gorkabmc <gorkabmc@gmail.com>
2017-03-11 20:16:00 +0100

    Array de 8 LEDs 4

--- repte_4/repte_4.ino
+++ repte_4/repte_4.ino
@@ -30,49 +30,71 @@ void loop()
   while (Serial.available() > 0) {
     B = Serial.parseInt();
     if (Serial.read() == '\n') {
- if (B == 0)
- {
- }
-  else if (B == 1)
-  {
-    digitalWrite(5, HIGH);
-    delay(1000);
-    digitalWrite(5, LOW);
-    delay(1000);
-  }
-    else if (B == 2)
-  {
-    digitalWrite(6, HIGH);
-    delay(1000);
-    digitalWrite(6, LOW);
-    delay(1000);
-  }
-      else if (B == 3)
-  {
-    digitalWrite(7, HIGH);
-    delay(1000);
-    digitalWrite(7, LOW);
-    delay(1000);
-  }
-      else if (B == 4)
-  {
-    digitalWrite(8, HIGH);
-    delay(1000);
-    digitalWrite(8, LOW);
-    delay(1000);
-  }
-        else if (B == 5)
-  {
-    digitalWrite(5, HIGH);
-    digitalWrite(7, HIGH);
-    delay(1000);
-    digitalWrite(5, LOW);
-    digitalWrite(7, LOW);
-    delay(1000);
-  }
+      if (B == 0)
+      {
       }
-  }
+      else if (B == 1)
+      {
+        digitalWrite(5, HIGH);
+        delay(1000);
+        digitalWrite(5, LOW);
+        delay(1000);
+      }
+      else if (B == 2)
+      {
+        digitalWrite(6, HIGH);
+        delay(1000);
+        digitalWrite(6, LOW);
+        delay(1000);
+      }
+      else if (B == 3)
+      {
+        digitalWrite(7, HIGH);
+        delay(1000);
+        digitalWrite(7, LOW);
+        delay(1000);
+      }
+      else if (B == 4)
+      {
+        digitalWrite(8, HIGH);
+        delay(1000);
+        digitalWrite(8, LOW);
+        delay(1000);
+      }
+      else if (B == 5)
+      {
+        digitalWrite(5, HIGH);
+        digitalWrite(7, HIGH);
+        delay(1000);
+        digitalWrite(5, LOW);
+        digitalWrite(7, LOW);
+        delay(1000);
+      }
+      else if (B == 6)
+      {
+       
+        digitalWrite(6, HIGH);
+        digitalWrite(7, HIGH);
+        delay(1000);
+       
+        digitalWrite(6, LOW);
+        digitalWrite(7, LOW);
+        delay(1000);
+      }
+            else if (B == 7)
+      {
+        digitalWrite(5, HIGH);
+        digitalWrite(6, HIGH);
+        digitalWrite(7, HIGH);
+        delay(1000);
+        digitalWrite(5, LOW);
+        digitalWrite(6, LOW);
+        digitalWrite(7, LOW);
+        delay(1000);
+      }
     }
-    
+  }
+}
 
-  //******************************* FUNCIONS ****************************************
+
+//******************************* FUNCIONS ****************************************
