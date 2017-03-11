e15489cf78185eeaa1cfcf8c25c1723ad5973f3d
Gorkabmc <gorkabmc@gmail.com>
2017-03-11 20:20:15 +0100

    Array de 8 LEDs 4

--- repte_4/repte_4.ino
+++ repte_4/repte_4.ino
@@ -72,16 +72,16 @@ void loop()
       }
       else if (B == 6)
       {
-       
+
         digitalWrite(6, HIGH);
         digitalWrite(7, HIGH);
         delay(1000);
-       
+
         digitalWrite(6, LOW);
         digitalWrite(7, LOW);
         delay(1000);
       }
-            else if (B == 7)
+      else if (B == 7)
       {
         digitalWrite(5, HIGH);
         digitalWrite(6, HIGH);
@@ -90,6 +90,13 @@ void loop()
         digitalWrite(5, LOW);
         digitalWrite(6, LOW);
         digitalWrite(7, LOW);
+        delay(1000);
+      }
+      else if (B == 8)
+      {
+        digitalWrite(8, HIGH);
+        delay(1000);
+        digitalWrite(8, LOW);
         delay(1000);
       }
     }
