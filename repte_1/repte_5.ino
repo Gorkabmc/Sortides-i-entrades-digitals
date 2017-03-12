d1209e8a227c0323d575205ec4c12e95fc98f279
Gorkabmc <gorkabmc@gmail.com>
2017-03-12 23:23:28 +0100

    Array de 8 LEDs 5

--- repte_1/repte_5.ino
+++ repte_1/repte_5.ino
@@ -8,30 +8,40 @@
 //******************************* INCLUDES ****************************************
 
 //******************************* VARIABLES ***************************************
-int LED5 = 5;
-int LED6 = 6;
-int LED7 = 7;
-int LED8 = 8;
-int B;
+const int ledPin[] = {5, 6, 7, 8};  
+const int buttonPin = 2;           
+boolean buttonEstat = LOW;          
+
+int ledNum = 4;                 
+int num = 0;                    
 //******************************** SETUP ******************************************
 void setup()
+void setup()
 {
-  Serial.begin(9600);
-  Serial.println("Entra numero !");
-  pinMode(8, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(5, OUTPUT);
+  for(int i = 0; i < ledNum; i++) 
+  {
+    pinMode(ledPin[i], OUTPUT);
+  }
+  pinMode(buttonPin, INPUT);     
+  for(int j = 0; j < ledNum ; j++)    
+    { 
+      digitalWrite(ledPin[j], 0);
+    }
 }
 
 //********************************* LOOP ******************************************
 void loop()
 {
-  while (Serial.available() > 0) {
-    B = Serial.parseInt();
-    if (Serial.read() == '\n') {
-
+  buttonEstat = digitalRead(buttonPin);
+  if (buttonEstat == HIGH)
+  { 
+    num = num + 1;               
+    if (num == 16) num = 0;    
+    for(int j = 0; j < ledNum ; j++)  
+    { 
+      digitalWrite(ledPin[j], bitRead(num, j));
     }
+    delay(500);    
   }
 }
 
