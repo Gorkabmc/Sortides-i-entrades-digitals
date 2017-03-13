2326d4032140e290be72482e79bd7ea7cac1c3f7
Gorkabmc <gorkabmc@gmail.com>
2017-03-13 21:28:44 +0100

    cruilla final

--- Cru_lla_de_sem_fors/Cru_lla_de_sem_fors.ino
+++ Cru_lla_de_sem_fors/Cru_lla_de_sem_fors.ino
@@ -8,62 +8,66 @@
 //******************************* INCLUDES ****************************************
 
 //********** Variables ************************************************************
-const int semaforAR = 7;          
-const int semaforAT = 8;          
-const int semaforAV = 9;          
-const int semaforBR = 10;         
-const int semaforBT = 11;         
-const int semaforBV = 12;        
-int pausa = 500;  
-//******************************** SETUP ******************************************
+const int semaforAR = 7;          // donar nom al pin 7 de l’Arduino
+const int semaforAT = 8;          // donar nom al pin 8 de l’Arduino
+const int semaforAV = 9;          // donar nom al pin 9 de l’Arduino
+const int semaforBR = 10;         // donar nom al pin 10 de l’Arduino
+const int semaforBT = 11;         // donar nom al pin 11 de l’Arduino
+const int semaforBV = 12;         // donar nom al pin 12 de l’Arduino
+int pausa = 500;             // definir variable de temps en ms
+
+//********** Setup ****************************************************************
 void setup()
 {
-  pinMode(semaforAR, OUTPUT); 
-  pinMode(semaforAT, OUTPUT);    
-  pinMode(semaforAV, OUTPUT);    
-  pinMode(semaforBR, OUTPUT);     
-  pinMode(semaforBT, OUTPUT);     
-  pinMode(semaforBV, OUTPUT);     
-  digitalWrite(semaforAR, HIGH);    
-  digitalWrite(semaforAT, LOW);    
-  digitalWrite(semaforAV, LOW);   
-  digitalWrite(semaforBR, HIGH);    
-  digitalWrite(semaforBT, LOW);    
-  digitalWrite(semaforBV, LOW);     
+  pinMode(semaforAR, OUTPUT);     // definir el pin 7 com una sortida
+  pinMode(semaforAT, OUTPUT);     // definir el pin 8 com una sortida
+  pinMode(semaforAV, OUTPUT);     // definir el pin 9 com una sortida
+  pinMode(semaforBR, OUTPUT);     // definir el pin 10 com una sortida
+  pinMode(semaforBT, OUTPUT);     // definir el pin 11 com una sortida
+  pinMode(semaforBV, OUTPUT);     // definir el pin 12 com una sortida
   
-  delay(pausa);                     
+  digitalWrite(semaforAR, HIGH);    // posar a 5V el pin 7
+  digitalWrite(semaforAT, LOW);    // posar a 0V el pin 8
+  digitalWrite(semaforAV, LOW);    // posar a 0V el pin 9
+  digitalWrite(semaforBR, HIGH);    // posar a 5V el pin 10
+  digitalWrite(semaforBT, LOW);    // posar a 0V el pin 11
+  digitalWrite(semaforBV, LOW);    // posar a 0V el pin 12  
+  
+  delay(pausa);                     // es queden leds pausa ms en aquest estat
 }
-//********************************* LOOP ******************************************
+
+//********** Loop *****************************************************************
 void loop()
 {
-  digitalWrite(semaforBR, LOW);     
-  digitalWrite(semaforBV, HIGH);    
+  digitalWrite(semaforBR, LOW);     // apaga roig semàfor B
+  digitalWrite(semaforBV, HIGH);    // encén verd semàfor B
   
-  delay(6*pausa);                   
+  delay(4*pausa);                   // es queden leds 4*pausa ms en aquest estat
   
-  digitalWrite(semaforBV, LOW);     
-  digitalWrite(semaforBT, HIGH);    
+  digitalWrite(semaforBV, LOW);     // apaga verd semàfor B
+  digitalWrite(semaforBT, HIGH);    // encén taronja semàfor B
   
-  delay(pausa);                     
+  delay(pausa);                     // es queden leds pausa ms en aquest estat
   
-  digitalWrite(semaforBT, LOW);     
-  digitalWrite(semaforBR, HIGH);    
+  digitalWrite(semaforBT, LOW);     // apaga taronja semàfor B
+  digitalWrite(semaforBR, HIGH);    // encén roig semàfor B
    
-  delay(pausa);                     
+  delay(pausa);                     // es queden leds pausa ms en aquest estat
   
-  digitalWrite(semaforAR, LOW);     
-  digitalWrite(semaforAV, HIGH);    
+  digitalWrite(semaforAR, LOW);     // apaga roig semàfor A
+  digitalWrite(semaforAV, HIGH);    // encén verd semàfor A
   
-  delay(6*pausa);                   
+  delay(4*pausa);                   // es queden leds 4*pausa ms en aquest estat
   
-  digitalWrite(semaforAV, LOW);    
-  digitalWrite(semaforAT, HIGH);    
+  digitalWrite(semaforAV, LOW);     // apaga verd semàfor A
+  digitalWrite(semaforAT, HIGH);    // encén taronja semàfor A
   
-  delay(pausa);                     
+  delay(pausa);                     // es queden leds pausa ms en aquest estat
   
-  digitalWrite(semaforAT, LOW);     
-  digitalWrite(semaforAR, HIGH);    
+  digitalWrite(semaforAT, LOW);     // apaga taronja semàfor A
+  digitalWrite(semaforAR, HIGH);    // encén roig semàfor A
   
-   delay(pausa);                     
+   delay(pausa);                     // es queden leds pausa ms en aquest estat
 }
-//******************************* FUNCIONS ****************************************
+
+//********** Funcions *************************************************************
