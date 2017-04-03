f70d7bad51a0c75d2d414c9157679adcab5765a4
Gorkabmc <gorkabmc@gmail.com>
2017-04-03 23:10:40 +0200

    test

--- Proba_1_cub_3x3x3/Proba_1_cub_3x3x3.ino
+++ Proba_1_cub_3x3x3/Proba_1_cub_3x3x3.ino
@@ -44,11 +44,32 @@ void setup()
   pinMode( FIL1 , OUTPUT );// 8 fila 1
   pinMode( COL9 , OUTPUT );
   pinMode( FIL1 , OUTPUT );// 9 fila 1
+
+  pinMode( COL1 , OUTPUT );
+  pinMode( FIL2 , OUTPUT );// 1 fila 2
+  pinMode( COL2 , OUTPUT );
+  pinMode( FIL2 , OUTPUT );// 2 fila 2
+  pinMode( COL3 , OUTPUT );
+  pinMode( FIL2 , OUTPUT );// 3 fila 2
+  pinMode( COL4 , OUTPUT );
+  pinMode( FIL2 , OUTPUT );// 4 fila 2
+  pinMode( COL5 , OUTPUT );
+  pinMode( FIL2 , OUTPUT );// 5 fila 2
+  pinMode( COL6 , OUTPUT );
+  pinMode( FIL2 , OUTPUT );// 6 fila 2
+  pinMode( COL7 , OUTPUT );
+  pinMode( FIL2 , OUTPUT );// 7 fila 2
+  pinMode( COL8 , OUTPUT );
+  pinMode( FIL2 , OUTPUT );// 8 fila 2
+  pinMode( COL9 , OUTPUT );
+  pinMode( FIL2 , OUTPUT );// 9 fila 2
 }
 
 //********************************* LOOP ******************************************
 void loop()
 {
+  // PRIMERA PLANTA
+
   // 1 fila 1
   digitalWrite (COL1, HIGH);
   digitalWrite (FIL1, HIGH);
@@ -115,6 +136,76 @@ void loop()
   delay(A);
   digitalWrite (COL9, LOW);
   digitalWrite (FIL1, LOW);
+  delay(A);
+
+  // SEGONA PLANTA
+
+  // 1 fila 2
+  digitalWrite (COL1, HIGH);
+  digitalWrite (FIL2, HIGH);
+  delay(A);
+  digitalWrite (COL1, LOW);
+  digitalWrite (FIL2, LOW);
+  delay(A);
+
+  // 2 fila 2
+  digitalWrite (COL2, HIGH);
+  digitalWrite (FIL2, HIGH);
+  delay(A);
+  digitalWrite (COL2, LOW);
+  digitalWrite (FIL2, LOW);
+  delay(A);
+
+  // 3 fila 2
+  digitalWrite (COL3, HIGH);
+  digitalWrite (FIL2, HIGH);
+  delay(A);
+  digitalWrite (COL3, LOW);
+  digitalWrite (FIL2, LOW);
+  delay(A);
+
+  // 4 fila 2
+  digitalWrite (COL4, HIGH);
+  digitalWrite (FIL2, HIGH);
+  delay(A);
+  digitalWrite (COL4, LOW);
+  digitalWrite (FIL2, LOW);
+  delay(A);
+
+  // 5 fila 2
+  digitalWrite (COL5, HIGH);
+  digitalWrite (FIL2, HIGH);
+  delay(A);
+  digitalWrite (COL5, LOW);
+  digitalWrite (FIL2, LOW);
+  delay(A);
+  // 6 fila 2
+  digitalWrite (COL6, HIGH);
+  digitalWrite (FIL2, HIGH);
+  delay(A);
+  digitalWrite (COL6, LOW);
+  digitalWrite (FIL2, LOW);
+  delay(A);
+  // 7 fila 2
+  digitalWrite (COL7, HIGH);
+  digitalWrite (FIL2, HIGH);
+  delay(A);
+  digitalWrite (COL7, LOW);
+  digitalWrite (FIL2, LOW);
+  delay(A);
+  // 8 fila 2
+  digitalWrite (COL8, HIGH);
+  digitalWrite (FIL2, HIGH);
+  delay(A);
+  digitalWrite (COL8, LOW);
+  digitalWrite (FIL2, LOW);
+  delay(A);
+  // 9 fila 2
+  digitalWrite (COL9, HIGH);
+  digitalWrite (FIL2, HIGH);
+  delay(A);
+  digitalWrite (COL9, LOW);
+  digitalWrite (FIL2, LOW);
   delay(A);
 }
 
