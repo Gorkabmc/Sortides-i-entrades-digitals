a4d1a7aba9f5c153c6ae9c3da58f913323378e5d
Gorkabmc <gorkabmc@gmail.com>
2017-04-03 23:15:11 +0200

    TEST OK !

--- Proba_1_cub_3x3x3/Proba_1_cub_3x3x3.ino
+++ Proba_1_cub_3x3x3/Proba_1_cub_3x3x3.ino
@@ -18,7 +18,7 @@ int COL9 = 2;
 int FIL1 = 11;
 int FIL2 = 12;
 int FIL3 = 13;
-int A = 500;
+int A = 400;
 //******************************* VARIABLES ***************************************
 
 //******************************** SETUP ******************************************
@@ -63,6 +63,25 @@ void setup()
   pinMode( FIL2 , OUTPUT );// 8 fila 2
   pinMode( COL9 , OUTPUT );
   pinMode( FIL2 , OUTPUT );// 9 fila 2
+
+  pinMode( COL1 , OUTPUT );
+  pinMode( FIL3 , OUTPUT );// 1 fila 3
+  pinMode( COL2 , OUTPUT );
+  pinMode( FIL3 , OUTPUT );// 2 fila 3
+  pinMode( COL3 , OUTPUT );
+  pinMode( FIL3 , OUTPUT );// 3 fila 3
+  pinMode( COL4 , OUTPUT );
+  pinMode( FIL3 , OUTPUT );// 4 fila 3
+  pinMode( COL5 , OUTPUT );
+  pinMode( FIL3 , OUTPUT );// 5 fila 3
+  pinMode( COL6 , OUTPUT );
+  pinMode( FIL3 , OUTPUT );// 6 fila 3
+  pinMode( COL7 , OUTPUT );
+  pinMode( FIL3 , OUTPUT );// 7 fila 3
+  pinMode( COL8 , OUTPUT );
+  pinMode( FIL3 , OUTPUT );// 8 fila 3
+  pinMode( COL9 , OUTPUT );
+  pinMode( FIL3 , OUTPUT );// 9 fila 3
 }
 
 //********************************* LOOP ******************************************
@@ -179,6 +198,7 @@ void loop()
   digitalWrite (COL5, LOW);
   digitalWrite (FIL2, LOW);
   delay(A);
+  
   // 6 fila 2
   digitalWrite (COL6, HIGH);
   digitalWrite (FIL2, HIGH);
@@ -186,13 +206,15 @@ void loop()
   digitalWrite (COL6, LOW);
   digitalWrite (FIL2, LOW);
   delay(A);
-  // 7 fila 2
+  
+  // 7 fila 2  
   digitalWrite (COL7, HIGH);
   digitalWrite (FIL2, HIGH);
   delay(A);
   digitalWrite (COL7, LOW);
   digitalWrite (FIL2, LOW);
   delay(A);
+  
   // 8 fila 2
   digitalWrite (COL8, HIGH);
   digitalWrite (FIL2, HIGH);
@@ -200,6 +222,7 @@ void loop()
   digitalWrite (COL8, LOW);
   digitalWrite (FIL2, LOW);
   delay(A);
+  
   // 9 fila 2
   digitalWrite (COL9, HIGH);
   digitalWrite (FIL2, HIGH);
@@ -207,6 +230,81 @@ void loop()
   digitalWrite (COL9, LOW);
   digitalWrite (FIL2, LOW);
   delay(A);
+
+  // TERCERA PLANTA
+
+  // 1 fila 3
+  digitalWrite (COL1, HIGH);
+  digitalWrite (FIL3, HIGH);
+  delay(A);
+  digitalWrite (COL1, LOW);
+  digitalWrite (FIL3, LOW);
+  delay(A);
+
+  // 2 fila 3
+  digitalWrite (COL2, HIGH);
+  digitalWrite (FIL3, HIGH);
+  delay(A);
+  digitalWrite (COL2, LOW);
+  digitalWrite (FIL3, LOW);
+  delay(A);
+
+  // 3 fila 3
+  digitalWrite (COL3, HIGH);
+  digitalWrite (FIL3, HIGH);
+  delay(A);
+  digitalWrite (COL3, LOW);
+  digitalWrite (FIL3, LOW);
+  delay(A);
+
+  // 4 fila 3
+  digitalWrite (COL4, HIGH);
+  digitalWrite (FIL3, HIGH);
+  delay(A);
+  digitalWrite (COL4, LOW);
+  digitalWrite (FIL3, LOW);
+  delay(A);
+
+  // 5 fila 3
+  digitalWrite (COL5, HIGH);
+  digitalWrite (FIL3, HIGH);
+  delay(A);
+  digitalWrite (COL5, LOW);
+  digitalWrite (FIL3, LOW);
+  delay(A);
+  
+  // 6 fila 3
+  digitalWrite (COL6, HIGH);
+  digitalWrite (FIL3, HIGH);
+  delay(A);
+  digitalWrite (COL6, LOW);
+  digitalWrite (FIL3, LOW);
+  delay(A);
+  
+  // 7 fila 3
+  digitalWrite (COL7, HIGH);
+  digitalWrite (FIL3, HIGH);
+  delay(A);
+  digitalWrite (COL7, LOW);
+  digitalWrite (FIL3, LOW);
+  delay(A);
+  
+  // 8 fila 3
+  digitalWrite (COL8, HIGH);
+  digitalWrite (FIL3, HIGH);
+  delay(A);
+  digitalWrite (COL8, LOW);
+  digitalWrite (FIL3, LOW);
+  delay(A);
+  
+  // 9 fila 3
+  digitalWrite (COL9, HIGH);
+  digitalWrite (FIL3, HIGH);
+  delay(A);
+  digitalWrite (COL9, LOW);
+  digitalWrite (FIL3, LOW);
+  delay(A);
+
 }
 
 //******************************* FUNCIONS ****************************************
